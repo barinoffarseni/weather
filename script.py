84fbe69fc4f1948e392297c56d0f8072f1d6232e
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2023-08-29 15:09:09 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,66 +1,86 @@
-
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLineEdit, QLabel
 import sys 
 import os
-from dotenv import load_dotenv 
 import requests
+from dotenv import load_dotenv 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLineEdit, QComboBox, QLabel
 
 load_dotenv()
 
-key = os.getenv('KEY')
-
-
-class DataInputApp(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
+
+        self.key = os.getenv('KEY')
 
         self.setWindowTitle("Weather")
         self.setGeometry(200, 200, 400, 400)
 
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
         layout = QVBoxLayout()
-        central_widget.setLayout(layout)
 
-        self.data_input = QLineEdit()
-        layout.addWidget(self.data_input)
+        widget = QComboBox()
+        widget.setEditable(True)
+        widget.addItems(["Moscow", "Ottawa", "Paris"])
 
-        self.submit_button = QPushButton("Enter")
-        self.submit_button.clicked.connect(self.input_data)
-        layout.addWidget(self.submit_button)
+        # self.data_input = QLineEdit()
+        
+        # self.addItems(["One", "Two", "Three"])
+        layout.addWidget(widget)
 
-        self.data_label = QLabel(":")
-        layout.addWidget(self.data_label)
+
+        submit_button = QPushButton("Enter")
+        submit_button.clicked.connect(self.click)
+        layout.addWidget(submit_button)
 
         self.data_temperature = QLabel(":")
         layout.addWidget(self.data_temperature)
 
-        self.data_observation_time = QLabel(":")
-        layout.addWidget(self.data_observation_time)
 
-        self.data_query = QLabel(":")
-        layout.addWidget(self.data_query)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-    def input_data(self,):
-        city = self.data_input.text()
+    def click(self):
+        city = self.get_city()
+        temperature = self.get_temperature(city, self.key)
+        self.refresh_widget_temperature(temperature)
 
+    def get_city(widget):
+        return widget.text()
+
+    def get_temperature(self, city, key):
         url = 'http://api.weatherstack.com/current?query=' + city + '&access_key=' + key + '&units=m'
         res = requests.get(url)
-
-        print(res.text)
         json = res.json()
-        observation_time = json["current"]["observation_time"]
-        temperature = json["current"]["temperature"]
-        query = json["request"]["query"]
+        return json["current"]["temperature"]
 
-        self.data_label.setText(f"url: {url}")
+    def refresh_widget_temperature(self, temperature):
         self.data_temperature.setText(f"temperature: {temperature}")
-        self.data_observation_time.setText(f"observation_time: {observation_time}")
-        self.data_query.setText(f"query: {query}")
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = DataInputApp()
-    window.show()
-    sys.exit(app.exec())
+# class MainWindow(QMainWindow):
+
+#     def __init__(self):
+#         super(MainWindow, self).__init__()
+
+#         self.setWindowTitle("My App")
+
+#         widget = QComboBox()
+#         widget.addItems(["One", "Two", "Three"])
+
+#         # Отправляет текущий индекс (позицию) выбранного элемента.
+#         widget.currentIndexChanged.connect( self.index_changed )
+
+#         # # Есть альтернативный сигнал отправки текста.
+#         widget.text_changed.connect( self.text_changed )
+
+#         self.setCentralWidget(widget)
+
+    # def input_data(self):
+    #     city = self.data_input.text()
+
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
