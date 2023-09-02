f110b97a5df0c48c3dc6619df030ca9114ca8b43
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2023-09-02 16:36:35 +0300

    Update script.py

--- script.py
+++ script.py
@@ -9,6 +9,36 @@ from threading import Timer
 load_dotenv()
 
 class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.key = os.getenv('KEY')
+
+        self.setWindowTitle("Weather")
+        self.setGeometry(200, 200, 400, 400)
+
+
+        self.data_input = QComboBox()
+        self.data_input.setEditable(True)
+        self.data_input.addItems(["Moscow", "Ottawa", "Paris"])
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.data_input)
+        self.home = QLabel()
+        layout.addWidget(self.home)
+
+        submit_button = QPushButton("Enter")
+        submit_button.clicked.connect(self.click)
+        layout.addWidget(submit_button)
+
+        self.data_temperature = QLabel(":")
+        layout.addWidget(self.data_temperature)
+
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
     def temperature_timer(self):
         temperature = self.get_temperature(self.city, self.key)
         self.refresh_widget_temperature(temperature)
@@ -29,44 +59,12 @@ class MainWindow(QMainWindow):
         res = requests.get(url)
         json = res.json()
         return json["current"]["temperature"]
-    def __init__(self, temperature):
-        
-        super(MainWindow, self).__init__()
 
-        self.key = os.getenv('KEY')
-
-        self.setWindowTitle("Weather")
-        self.setGeometry(200, 200, 400, 400)
-
-        self.home = QLabel()
+    def refresh_widget_temperature(self, temperature):
         if temperature < 15:
             self.home.setPixmap(QPixmap('снежинка.png.'))
         elif temperature > 15:
             self.home.setPixmap(QPixmap('солнышко.png.'))
-
-        self.data_input = QComboBox()
-        self.data_input.setEditable(True)
-        self.data_input.addItems(["Moscow", "Ottawa", "Paris"])
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.data_input)
-        layout.addWidget(self.home)
-
-        submit_button = QPushButton("Enter")
-        submit_button.clicked.connect(self.click)
-        layout.addWidget(submit_button)
-
-        self.data_temperature = QLabel(":")
-        layout.addWidget(self.data_temperature)
-
-
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-        return temperature
-
-
-    def refresh_widget_temperature(self, temperature):
         self.data_temperature.setText(f"temperature: {temperature}")
 
 
